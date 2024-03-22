d7064c7caae1b18c4e89c4ced10277c68495c531
chiragali <chirag.ali47@gmail.com>
2024-03-22 16:46:22 +0530

    Git Test

--- src/main/java/base/AppConstants.java
+++ src/main/java/base/AppConstants.java
@@ -3,6 +3,6 @@ package base;
 public class AppConstants {
 
     public static final String browserName = System.getProperty("browserName", "chrome");
-    public static final String platform = System.getProperty("platform", "remote_git");
+    public static final String platform = System.getProperty("platform", "remote");
 
 }
